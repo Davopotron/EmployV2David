1e92f7d9fcc47ba3c803e8e3955bd95005b4482d
David <davidopotowsky14@gmail.com>
2024-10-08 14:02:29 -0500

    added error 500

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.originalUrl}`);
+    next();
+});
+
 app.get("/", (req, res) => {
   res.send("Hello employees!");
 });
@@ -27,8 +34,14 @@ app.get("/employees/:id", (req, res) => {
   }
 });
 
-app.arguments((req, res, next) => {
+app.use((req, res, next) => {
   next({ status: 404, message: "Endpoint not found"});
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.status ?? 500);
+  res.json(err.message ?? "Sorry, something went wrong!");
 });
 
 app.listen(PORT, () => {
