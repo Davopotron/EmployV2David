0b4dd4047c9bdbb9f863ac876d8e07d35d27a8ed
David <davidopotowsky14@gmail.com>
2024-10-10 18:24:27 -0500

    added new employee function

--- api/employee-router.js
+++ api/employee-router.js
@@ -2,10 +2,21 @@ const express = require("express");
 const router = express.Router();
 module.exports = router;
 
-const employees = require("../employees");
+const employees = require("../data/employees");
 
 router.get("/", (req, res) => {
   res.json(employees);
+});
+
+router.post("/", (req, res, next) => {
+  const { name } = req.body;
+  if (name) {
+    const newEmployee = { id: employees.length + 1, name };
+    employees.push(newEmployee);
+    res.status(201).json(newEmployee);
+  } else {
+    next({ status: 400, message: "New employee must have a name." });
+  }
 });
 
 router.get("/random", (req, res) => {
