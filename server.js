23cc4ccffcf4f2a7d26fe2c1cbccba156fffa675
David <davidopotowsky14@gmail.com>
2024-10-10 18:14:18 -0500

    neutralized extraneous code

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const PORT = 3000;
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.originalUrl}`);
-    next();
-});
+// app.use((req, res, next) => {
+//   console.log(`${req.method} ${req.originalUrl}`);
+//     next();
+// });
 
 app.get("/", (req, res) => {
   res.send("Hello employees!");
